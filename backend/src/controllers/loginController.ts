316aa0ed5904c9aa17a99d607c733227ef410b9d
BirdTool <guilhermealvesga0789@hotmail.com>
2025-03-12 20:43:30 -0300

    back: login completed

--- backend/src/controllers/loginController.ts
+++ backend/src/controllers/loginController.ts
@@ -2,7 +2,7 @@ import { Context } from 'hono';
 import * as jwt from 'jsonwebtoken';
 import pool from '../services/db';
 import * as crypto from 'crypto';
-import { loginSchemaStudent } from '../schemas/loginSchema';
+import { loginSchemaStudent, loginSchemaTeacher, loginSchemaManagement } from '../schemas/loginSchema';
 
 const SECRET_KEY: string = process.env.SECRET_KEY as string;
 
@@ -70,6 +70,80 @@ export const loginController = async (c: Context) => {
                 );
             } catch (error) {
                 console.error("Erro ao verificar estudante:", error);
+                return c.json({ success: false, message: 'Erro interno no servidor' }, 500);
+            }
+        }
+        case 'teacher': {
+            const validatedData = loginSchemaTeacher.safeParse(body);
+            if (!validatedData.success) {
+                return c.json({ success: false, message: 'Dados inválidos', errors: validatedData.error.errors }, 400);
+            }
+
+            const { email, password } = validatedData.data;
+
+            try {
+                const teacherCheck = await pool.query(
+                    "SELECT id, nome, email, senha FROM teachers WHERE email = $1 AND senha = $2",
+                    [email, password]
+                );                
+
+                if (teacherCheck.rowCount === 0) {
+                    return c.json({ success: false, message: 'Professor não encontrado ou senha incorreta' }, 404);
+                }
+
+                const teacher = <{id: number, nome: string, email: string, senha: string}>teacherCheck.rows[0];
+
+                const token = jwt.sign(
+                    { id: teacher.id, name: teacher.nome, email: teacher.email, password: teacher.senha, role: 'professor' },
+                    SECRET_KEY,
+                    { expiresIn: '1h' }
+                );
+
+                const csrfToken = crypto.randomBytes(32).toString('hex');
+                c.header('Set-Cookie', `token=${token}; HttpOnly; Path=/; SameSite=Strict; Max-Age=3600`); // adicionar Secure após HttpOnly em produção
+                return c.json(
+                    { success: true, message: 'Login bem-sucedido', data: { name: teacher.nome, email: teacher.email, csrfToken } },
+                    200
+                );
+            } catch (err) {
+                console.log("Erro ao verificar professor:", err);
+                return c.json({ success: false, message: 'Erro interno no servidor' }, 500);
+            }
+        }
+        case 'management': {
+            const validatedData = loginSchemaManagement.safeParse(body);
+            if (!validatedData.success) {
+                return c.json({ success: false, message: 'Dados inválidos', errors: validatedData.error.errors }, 400);
+            }
+
+            const { email, password } = validatedData.data;
+
+            try {
+                const managementCheck = await pool.query(
+                    "SELECT id, nome, email, senha, cargo FROM management WHERE email = $1 AND senha = $2",
+                    [email, password]
+                );                
+
+                if (managementCheck.rowCount === 0) {
+                    return c.json({ success: false, message: 'Professor não encontrado ou senha incorreta' }, 404);
+                }
+
+                const management = <{id: number, nome: string, email: string, senha: string, cargo: string}>managementCheck.rows[0];
+
+                const token = jwt.sign(
+                    { id: management.id, name: management.nome, email: management.email, password: management.senha, role: management.cargo },
+                    SECRET_KEY,
+                    { expiresIn: '1h' }
+                );
+
+                const csrfToken = crypto.randomBytes(32).toString('hex');
+                c.header('Set-Cookie', `token=${token}; HttpOnly; Path=/; SameSite=Strict; Max-Age=3600`); // adicionar Secure após HttpOnly em produção
+                return c.json(
+                    { success: true, message: 'Login bem-sucedido', data: { name: management.nome, email: management.email, role: management.cargo, csrfToken } },
+                    200
+                );
+            } catch (err) {
+                console.log("Erro ao verificar funcionário:", err);
                 return c.json({ success: false, message: 'Erro interno no servidor' }, 500);
             }
         }
